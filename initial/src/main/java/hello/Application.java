3da99682672753f79a54d0e056d5b1c8a2680502
Szymon Harabasz <szymon.harabasz@gmail.com>
2020-12-27 17:51:08 +0100

    Initial commit

--- initial/src/main/java/hello/Application.java
+++ initial/src/main/java/hello/Application.java
@@ -1,12 +1,58 @@
 package hello;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
+@RestController
 public class Application {
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+
+  private List<Contact> data;
+  public Application() {
+    data = Arrays.asList(
+      new Contact("Stefan Autorski","01234543210","al. Inna 12\nKrzyżówkowo\n11-111","steve228uk@gmail.com","stefanautorski.to",""),
+      new Contact("Janko Walski","0123456789","ul. Zagadkowa 123\nSzarada Duża\n10-010 Polska","janko@walski.com","http://janko-walski.info","Kilka słów na temat Janko."),
+      new Contact("Karen Nerka","2345678","ul. Nieistniejąca 21\nBrystol\n12-332 Polska","karenne@email.com","karenne.com","")
+    );
+  }
+
+  @GetMapping("/contacts")
+  @CrossOrigin(origins = "http://localhost:3000")
+  public List<Contact> getAllContacts() {
+    System.out.println("Size now " + data.size());
+    return data;
+  }
+  @GetMapping("/contacts/{id}")
+  @CrossOrigin(origins = "http://localhost:3000")
+  public Contact getContact(@PathVariable("id") String id) {
+    return data.stream().filter(c -> c.getId().equals(id)).findFirst().orElseThrow();
+  }
+  @DeleteMapping("/contacts/{id}")
+  @CrossOrigin(origins = "http://localhost:3000")
+  public void deleteContact(@PathVariable("id") String id) {
+    System.out.println("Removing item with id " + id);
+    System.out.println("Size before " + data.size());
+    data = data.stream().filter(c -> !c.getId().equals(id)).collect(Collectors.toList());
+    System.out.println("Size after " + data.size());
+  }
+  @PutMapping("/contacts")
+  @CrossOrigin(origins = "http://localhost:3000")
+  public void putContact(@RequestParam Contact contact) {
+    System.out.println("Adding new contact " + contact);
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
 
 }
---
+++ initial/src/main/java/hello/Contact.java
@@ -0,0 +1,66 @@
+package hello;
+
+import java.util.UUID;
+
+public class Contact {
+    private String id;
+    private String name;
+    private String phone;
+    private String address;
+    private String email;
+    private String website;
+    private String notes;
+
+    public Contact() {
+        this("","","","","","");
+    }
+    public Contact(String name, String phone, String address, String email, String website, String notes) {
+        this.id = UUID.randomUUID().toString();
+        this.name = name;
+        this.phone = phone;
+        this.address = address;
+        this.email = email;
+        this.website = website;
+        this.notes = notes;
+    }
+
+    public String getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String s) {
+        name = s;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String s) {
+        phone = s;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String s) {
+        address = s;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String s) {
+        email = s;
+    }
+    public String getWebsie() {
+        return website;
+    }
+    public void setWebsite(String s) {
+        website = s;
+    }
+    public String getNotes() {
+        return notes;
+    }
+    public void setNotes(String s) {
+        notes = s;
+    }
+}
